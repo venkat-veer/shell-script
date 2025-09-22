e985bba067a4a0a29889b8588ef810024330e3f7
venkat-veer <s.venkatveer@gmail.com>
2025-09-23 04:17:55 +0530

    some message

--- 1-hello-world.sh
+++ 1-hello-world.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 
-
 #this is comment, cant be executed, just for information purpose
 echo "hello world"
---
+++ 2-conversion.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+echo "Ramesh:: Hi Suresh How are you?"
+echo "Suresh:: Hello Ramesh, Iam fine , How are you?"
+echo "Ramesh:: what are you learning recently?"
+echo "Suresh:: Iam learning shell scripting Ramesh"
