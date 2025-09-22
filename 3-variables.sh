b22e01fcb32b6ac37192147cba3746bf46d9c2d5
venkat-veer <s.venkatveer@gmail.com>
2025-09-23 04:30:18 +0530

    some message

---
+++ 3-variables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+
+person1=John
+person2=Don
+
+echo "$person1:: Hi $person2 How are you?"
+echo "$person2:: Hello $person1, Iam fine , How are you?"
+echo "$person1:: what are you learning recently?"
+echo "$person2:: Iam learning shell scripting $person1"
