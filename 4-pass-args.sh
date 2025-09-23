75cb77b937d92fe3cd0dfeceded7cd8ae5e8228b
venkat-veer <s.venkatveer@gmail.com>
2025-09-23 11:54:16 +0530

    variables-args

---
+++ 4-pass-args.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hi $person2 How are you?"
+echo "$person2:: Hello $person1, Iam fine , How are you?"
+echo "$person1:: what are you learning recently?"
+echo "$person2:: Iam learning shell scripting $person1"
