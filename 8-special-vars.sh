7fb9f53a052b727996d3776be68d4143d035ccc9
venkat-veer <s.venkatveer@gmail.com>
2025-09-23 20:14:10 +0530

    variables-args

--- 8-special-vars.sh
+++ 8-special-vars.sh
@@ -3,4 +3,6 @@
 echo "All variables passed to the script:$@"
 echo "All variables passed to the script:$*"
 
- echo "script name:$0"
+ echo "script name:$0"                              # gives script name ok
+ echo "current working directory:$pwd"
+ echo "whoo is running this:$user"
