665a8e1a6421b62cf964f84eda0dc2cb5c5c6119
venkat-veer <s.venkatveer@gmail.com>
2025-09-23 12:10:25 +0530

    variables-args

--- 4-pass-args.sh
+++ 4-pass-args.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
+# Arguments
 person1=$1
-person2=$2
+person2=$2 
 
 echo "$person1:: Hi $person2 How are you?"
 echo "$person2:: Hello $person1, Iam fine , How are you?"
---
+++ 5-read-pin.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+echo "Please enter your pin number"
+read PIN_NUMBER                         # pin - number is variable 
+
+echo "PIN entered is: $PIN_NUMBER"
