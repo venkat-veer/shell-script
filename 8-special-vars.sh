b47300a7eff8e76acde62e065d41b8c8f5baa390
venkat-veer <s.venkatveer@gmail.com>
2025-09-23 20:20:40 +0530

    variables-args

--- 8-special-vars.sh
+++ 8-special-vars.sh
@@ -4,6 +4,7 @@ echo "All variables passed to the script:$@"
 echo "All variables passed to the script:$*"
 
  echo "script name:$0"                              # gives script name ok
- echo "current working directory:$PWD"
- echo "whoo is running this:$USER"
- echo "home directory of user:$HOME"
+ echo "current working directory:$PWD"              # gives current directory path
+ echo "who is running this:$USER"                  # current running user
+ echo "home directory of user:$HOME"                # user home directory
+ echo "PID of the script:$$"
