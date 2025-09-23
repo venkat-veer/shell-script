7b199a85f4ad70b6b68e6092a20108ff0b4ddc77
venkat-veer <s.venkatveer@gmail.com>
2025-09-23 21:57:03 +0530

    data-types-arrays

--- 9-data-types.sh
+++ 9-data-types.sh
@@ -5,6 +5,11 @@
 number1=100
 number2=200
 Name=DevOps
-sum=$(($number1+$number2+$Name))
+sum=$(($number1+$number2))
+# sum=$(($number1+$number2+$Name))        # ignore name here because it is string right
 
 echo "sum is: ${sum}" 
+
+
+leaders=("Modi","Putin","Trudo","Trump")
+echo "All leaders: ${leaders}[@]"
