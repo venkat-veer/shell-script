c477b4546a326ae5615f4c01df715277d83c010c
venkat-veer <s.venkatveer@gmail.com>
2025-09-25 11:55:58 +0530

    colour-enhance

--- 15-color-enhance.sh
+++ 15-color-enhance.sh
@@ -22,23 +22,26 @@ validate(){                     # functions receive input through args just like
 
 }
 
-dnf install mysql -y
+dnf list installed mysql
 if [ $? -ne 0 ]; then
     dnf install mysql -y
+    validate $? "MYSQL"
 else
     echo -e "mysql already exist:$Y SKIPPING $N"
 fi
 
-dnf install nginx -y
+dnf list installed nginx
 if [ $? -ne 0 ]; then
     dnf install nginx -y
+    validate $? "NGINX"
 else
     echo -e "nginx already exist:$Y SKIPPING $N"
 fi
 
-dnf install python3 -y
+dnf list installed python3
 if [ $? -ne 0 ]; then
-    dnf install python -y
+    dnf install python3 -y
+    validate $? "python3"
 else
-    echo -e "python already exist:$Y SKIPPING $N"
+    echo -e "python3 already exist:$Y SKIPPING $N"
 fi
