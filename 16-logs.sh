c5f3cade46ba837c13c207fea2001b0dc1e84097
venkat-veer <s.venkatveer@gmail.com>
2025-09-25 14:00:52 +0530

    logs-add

--- 16-logs.sh
+++ 16-logs.sh
@@ -7,7 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 
 logs_folder="/var/log/shell-script"
-script_name=$(echo $0 | cut -d '.' -f1)
+script_name=$(echo $0 | cut -d "." -f1)
 log_file="$logs_folder/$script_name.log"
 
 mkdir -p $logs_folder
