d605a3a6e7f65d779604e61f4d0c9a288d5bca90
venkat-veer <s.venkatveer@gmail.com>
2025-09-25 19:39:52 +0530

    loops-extend

--- 18-loops-extend.sh
+++ 18-loops-extend.sh
@@ -29,5 +29,14 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 
 for package in $@
 do
-    echo "Package is: $package"
+    #check package already installed or not
+    dnf list installed $package &>> LOG_FILE
+
+    # if exit status is 0, already installed, -ne 0 need to install it.
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "$package already installed ... $Y SKIPPING $N"
+    fi
 done
