04c148fd3eb1431235e307cf56d8361298b6e30e
venkat-veer <s.venkatveer@gmail.com>
2025-09-25 09:32:29 +0530

    conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 number=$1
-if [ $number -lt 10; ]; then
+if [ $number -lt 10 ]; then
     echo "given number $number is less than 10"
 else
     echo "given number $number is greater than or equal to 10"
