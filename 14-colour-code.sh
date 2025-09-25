b05b590cb7f47c8b451baa3da816a8ea4df296e7
venkat-veer <s.venkatveer@gmail.com>
2025-09-25 11:37:11 +0530

    colour-code

--- 14-colour-code.sh
+++ 14-colour-code.sh
@@ -5,10 +5,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-echo -e "$G Hello World"
+echo -e "$G Hello World"            # if u dont mention $N the next output also follow same colour
 echo "Check this Colour"
 
-echo -e "$Y Hello World"
+echo -e "$Y Hello World $N"            # if u  mention $N the current output only follow applied colour
 echo "Check this Colour"
 
 echo -e "$N Hello World"
