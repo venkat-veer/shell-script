bd75623dfdeb04ae6d1aeb992dfc9721415dac96
venkat-veer <s.venkatveer@gmail.com>
2025-09-25 09:43:23 +0530

    conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/bash
 
+# -gt greater than      # -ne not eqaul to          # -lt  less than
+
+# number=$1
+# if [ $number -lt 10 ]; then
+#     echo "given number $number is less than 10"
+# else
+#     echo "given number $number is greater than or equal to 10"
+# fi
+
+
 number=$1
 if [ $number -lt 10 ]; then
     echo "given number $number is less than 10"
+elif [$number -eq 10 ]; then 
+    echo "given number $number is  equal to 10"
 else
     echo "given number $number is greater than or equal to 10"
 fi
 
-# -gt greater than
-# -ne not eqaul to
-# -lt  less than
+
 
