1b4494ffc7fabed74f7bb4853ec7e9d76f410757
venkat-veer <s.venkatveer@gmail.com>
2025-09-25 09:55:37 +0530

    conditions

---
+++ 11-conditions-even.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+echo "please enter number;"
+read number
+
+if [ (($number % 2)) -eq 0 ]; then
+    echo "given number $number is EVEN"
+else
+     echo "given number $number is ODD"
+    
