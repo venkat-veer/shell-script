4e07f4a8e11edf1f57ffd8a7694ce3c7574345fb
venkat-veer <s.venkatveer@gmail.com>
2025-09-25 10:45:28 +0530

    exit-code

--- 12-exit-code.sh
+++ 12-exit-code.sh
@@ -4,12 +4,14 @@ userid=$(id -u)
 
 if [ $userid -ne 0 ]; then
     echo "ERROR: Please run the script with ROOT Privilege"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Installing MYSQL is failure"
+    exit 1
 else
     echo "Installing MYSQL is Success"
 fi
