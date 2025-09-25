1fe7ae185e8fa652305087fe416836fa7ef09f3a
venkat-veer <s.venkatveer@gmail.com>
2025-09-25 14:08:56 +0530

    logs-add

--- 16-logs.sh
+++ 16-logs.sh
@@ -11,7 +11,7 @@ script_name=$(echo $0 | cut -d "." -f1)
 log_file="$logs_folder/$script_name.log"
 
 mkdir -p $logs_folder
-echo "Script started executed at:$(date)"
+echo "Script started executed at:$(date)" | tee -a $log_file
 
 if [ $userid -ne 0 ]; then
     echo "ERROR: Please run the script with ROOT Privilege"
@@ -20,10 +20,10 @@ fi
 
 validate(){                     # functions receive input through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 is:$R failure $N"
+        echo -e "Installing $2 is:$R failure $N" | tee -a $log_file
         exit 1
     else
-        echo -e "Installing $2 is:$G is Success $N"
+        echo -e "Installing $2 is:$G is Success $N" | tee -a $log_file
     fi
 
 }
@@ -33,7 +33,7 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y &>>log_file 
     validate $? "MYSQL"
 else
-    echo -e "mysql already exist:$Y SKIPPING $N"
+    echo -e "mysql already exist:$Y SKIPPING $N"    | tee -a $log_file
 fi
 
 dnf list installed nginx &>>log_file 
@@ -41,7 +41,7 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y &>>log_file 
     validate $? "NGINX"
 else
-    echo -e "nginx already exist:$Y SKIPPING $N"
+    echo -e "nginx already exist:$Y SKIPPING $N"    | tee -a $log_file
 fi
 
 dnf list installed python3 &>>log_file 
@@ -49,5 +49,5 @@ if [ $? -ne 0 ]; then
     dnf install python3 -y &>>log_file 
     validate $? "python3"
 else
-    echo -e "python3 already exist:$Y SKIPPING $N"
+    echo -e "python3 already exist:$Y SKIPPING $N" | tee -a $log_file   # tee -read print on screen and write to file
 fi
