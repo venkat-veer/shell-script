5438768e068786cc6b45357459b609a334a649a9
venkat-veer <s.venkatveer@gmail.com>
2025-09-26 23:09:45 +0530

    error-handle

--- 19-set.sh
+++ 19-set.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
 set -e                                     # means exit when u got error.
-echo "Hello"
-echo "Before Error"
-configure please this one
+
+error(){
+    echo "There si an error"
+}
+
+trap error ERR 
+
+echo "Hello.."
+echo "Before Error.."
+abcdefghijkl;dnf            # here shell understands there is an error and signal is ERR.
 echo "After Error"
