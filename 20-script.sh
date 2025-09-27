0c1f3fb8148b6dfb27e662369ce1d7fdee90167b
venkat-veer <s.venkatveer@gmail.com>
2025-09-27 16:40:45 +0530

    call-scripts

--- 20-script.sh
+++ 20-script.sh
@@ -3,4 +3,7 @@
 NAME=INDIA
 echo "My Country: $NAME"
 echo "PID of script-20: $$"
-sh 21-script.sh
+# sh 21-script.sh
+
+source ./21-script.sh           # . means scritp file is in current directory ok.
+
