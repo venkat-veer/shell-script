f326d3179198a5240fcd07dff4f2311eb39a6224
venkat-veer <s.venkatveer@gmail.com>
2025-09-28 22:59:47 +0530

    whiel-loop

--- 23-while-loop.sh
+++ 23-while-loop.sh
@@ -7,7 +7,7 @@ echo "Start Count Down.."
 while [ $count -ge 0 ]
 do
     echo "time left: $count"
-    sleep 1                  # pause for 1 second
+    sleep 2                  # pause for 1 second
     count=$((count - 1))     # Decrement the count
 done
 
