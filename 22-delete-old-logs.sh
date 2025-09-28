9b0a4267a2e4e06a1b65d6c0676a55c5d1f78831
venkat-veer <s.venkatveer@gmail.com>
2025-09-28 23:25:52 +0530

    delete-old-logs

--- 22-delete-old-logs.sh
+++ 22-delete-old-logs.sh
@@ -13,7 +13,7 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-SOURCE_DIR=/home/ec2-user/app-logs
+SOURCE_DIR=/home/ec2-user/app-logssss
 
 if [ ! -d $SOURCE_DIR ]; then                   # d means directory. if dir not exist in source-dir
     echo -e "ERROR:: $SOURCE_DIR does not exist"
@@ -25,4 +25,6 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)          # 
 while IFS= read -r filepath          # internal filed seperator
 do
     echo "Deleting the file: $filepath"
+    rm -rf $filepath
+    echo "Deleted the file: $filepath"
 done <<< $FILES_TO_DELETE       # for file < for multiple files or variables use <<< ok 
