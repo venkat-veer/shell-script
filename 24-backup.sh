b8709c018dc73c4d150bc0e452d09dc247199b12
venkat-veer <s.venkatveer@gmail.com>
2025-09-29 12:18:51 +0530

    backup-script-final

--- 24-backup.sh
+++ 24-backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-set -e                          # when error founds in script exit immediately
+# set -e                          # when error founds in script exit immediately
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -27,27 +27,50 @@ USAGE(){
     exit 1
 }
 
+### check SOURCE-DIR or DEST_DIR passed or not. ###
+
 if [ $# -lt 2 ]; then
     USAGE
 fi 
 
+### check SOURCE_DIR-dir exist or not ###
 if [ ! -d $SOURCE_DIR ]; then
     echo -e "$R:Source $SOURCE_DIR does not exist $N"
+    exit 1
 fi
 
+### check DEST_DIR exist or not ###
 if [ ! -d $DEST_DIR ]; then
     echo -e "$R:Destination $DEST_DIR does not exist $N"
+    exit 1
 fi
 
-
+### Find the files ###
 FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)   
 
 if [ ! -z "${FILES}" ]; then                                    #z -check files empty or not 
+    ### start archieving the files ###
     echo "Files found: $FILES"
     TIMESTAMP=$(date +%F-%H-%M)                                 # archieve to current date-time
     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     echo "Zip file name: $ZIP_FILE_NAME"
-    find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS | zip -@ -j "$ZIP_FILE_NAME"
+    find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS | zip -@ -j "$ZIP_FILE_NAME"       # j without full-path only file name
+
+    ### check archieval success or not ###
+    if [ -f $ZIP_FILE_NAME ]
+    then
+        echo -e "Archieval ... $G SUCCESS $N"
+
+        ### Delete if success ###
+        while IFS= read -r filepath
+        do
+            echo "Deleting the File: $filepath"
+            rm -rf $filepath
+            echo "Deleted the file: $filepath"
+        done <<< $FILES
+    else
+            echo -e "Archieval ... $G SUCCESS $N"
+            exit 1
 else
-    echo -e "No Files to archieve ... $Y SKIPPING $N"
+        echo -e "No Files to archieve ... $Y SKIPPING $N"
 fi
