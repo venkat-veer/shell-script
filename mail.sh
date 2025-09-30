943df5c58bcf7ebf8033d2873a2d2b90ea306e6d
venkat-veer <s.venkatveer@gmail.com>
2025-09-30 15:14:30 +0530

    disk-alert-mail

--- mail.sh
+++ mail.sh
@@ -3,7 +3,8 @@
 TO_ADDRESS=$1
 SUBJECT=$2
 ALERT_TYPE=$3
-MESSAGE_BODY=$(printf '%s\n' | sed -e "s/'/'\\\\''/g; 1s/^/'; \$s/\$/'/")
+MESSAGE_BODY=$4
+FORMATTTED_BODY=$(printf '%s\n' $MESSAGE_BODY | sed -e "s/'/'\\\\''/g; 1s/^/'; \$s/\$/'/")
 IP_ADDRESS=$5 
 TO_TEAM=$6
 
