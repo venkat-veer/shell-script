743341594fe3e63c6b02b9f43447249d5f63c93e
venkat-veer <s.venkatveer@gmail.com>
2025-09-30 15:18:01 +0530

    disk-alert-mail

--- mail.sh
+++ mail.sh
@@ -4,11 +4,11 @@ TO_ADDRESS=$1
 SUBJECT=$2
 ALERT_TYPE=$3
 MESSAGE_BODY=$4
-FORMATTTED_BODY=$(printf '%s\n' $MESSAGE_BODY | sed -e "s/'/'\\\\''/g; 1s/^/'; \$s/\$/'/")
+FORMATTTED_BODY=$(printf '%s\n' "$MESSAGE_BODY" | sed -e "s/'/'\\\\''/g; 1s/^/'; \$s/\$/'/")
 IP_ADDRESS=$5 
 TO_TEAM=$6
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$MESSAGE_BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$FORMATTTED_BODY/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
