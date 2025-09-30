3c722468a446619cd722db15b9bb75f09ea9dc7f
venkat-veer <s.venkatveer@gmail.com>
2025-09-30 12:57:18 +0530

    disk-usage-alert

--- 25-disk-usage-alert.sh
+++ 25-disk-usage-alert.sh
@@ -2,6 +2,8 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=3                                # in project we keep it as 75 ok
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+MESSAGE=""
 
 while IFS= read -r line
 do
@@ -9,6 +11,9 @@ do
     USAGE=$(echo $line | awk '{print $6}' | cut -d '%' -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "High Usage on $PARTITION: $USAGE"
+        # echo "High Usage on $PARTITION: $USAGE"
+        MESSAGE="High Disk Usage on $PARTITION: $USAGE %"
     fi
 done <<< $DISK_USAGE
+
+echo "Message Body: $MESSAGE"
